f4b1cd15a1bdd8e7a6f6890cc55b093bc0998bd5
Generetion <geraldcydonia@gmail.com>
2019-01-14 10:55:04 -0600

    Completed the module

--- jobs/app.py
+++ jobs/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route('/')
 @app.route('/jobs')
-def jobs();
+def jobs():
     return render_template('index.html')
